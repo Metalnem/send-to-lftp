7fb0677091dec175549de6621ce6f3729da384c4
Metalnem <metalnem@mijailovic.net>
2016-05-23 21:04:35 +0200

    Show authentication dialog when server returns 401 Unauthorized

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ chrome.Cu.importGlobalProperties(['fetch']);
 
 const contextMenu = require('sdk/context-menu');
 const notifications = require('sdk/notifications');
+const panels = require('sdk/panel');
 const prefs = require('sdk/simple-prefs');
 
 contextMenu.Item({
@@ -12,11 +13,33 @@ contextMenu.Item({
 	onMessage: onSendLink
 });
 
-function onSendLink(path) {
-	return sendLink(path).then(onSuccess).catch(onError);
+function onSendLink(path, username, password) {
+	return sendLink(path, username, password)
+		.then(response => onResponse(path, response))
+		.catch(onError);
 }
 
-function onSuccess(response) {
+function onResponse(path, response) {
+	if (response.status === 401) {
+		return new Promise((resolve, reject) => {
+			const panel = panels.Panel({
+				contentURL: './password.html',
+				contentScriptFile: './password.js',
+				width: 250,
+				height: 100,
+				onHide: () => reject(new Error('Authentication failed.'))
+			});
+
+			panel.port.on('login', data => {
+				resolve(onSendLink(path, data.username, data.password));
+				panel.hide();
+			});
+
+			panel.port.on('cancel', () => panel.hide());
+			panel.show();
+		});
+	}
+
 	if (!response.ok) {
 		return response.json().then(response => {
 			throw new Error(response.message);
